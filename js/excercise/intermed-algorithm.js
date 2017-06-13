571ae3d5a257b47fbc782b4be1db83c5f0188ac0
Anett_Soos <soosanett@gmail.com>
2017-06-13 16:42:38 +0100

    interm-alg roman number converter

--- js/excercise/intermed-algorithm.js
+++ js/excercise/intermed-algorithm.js
@@ -1,9 +1,10 @@
 var data = [
   sumAll([1, 4]),
-  diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5])
+  diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]),
+  convertToRoman(36)
 ];
 
-/*Return the sum of two arguments (numbers) and all numbers between them*/
+/*Return the sum of two arguments (numbers) and all numbers between them */
 function sumAll(arr) {
   var minArr = arr.reduce(function(a, b) {
     return Math.min(a, b);
@@ -21,7 +22,7 @@ function sumAll(arr) {
 
 /*Compare two arrays and return a new array with any items only found in 
 one of the two given arrays, but not both. In other words, return the 
-symmetric difference of the two arrays.*/
+symmetric difference of the two arrays */
 function diffArray(arr1, arr2) {
   function onlyUnique(value, index, self) {
     return self.indexOf(value) === index;
@@ -48,4 +49,82 @@ function diffArray(arr1, arr2) {
   }
 
   return newArrCopy1.concat(newArrCopy2);
+}
+
+/*Convert the given number into a roman numeral */
+function convertToRoman(num) {
+  var romanNumbers = [
+    {
+      0 : "",
+      1 : "I",
+      2 : "II",
+      3 : "III",
+      4 : "IV",
+      5 : "V",
+      6 : "VI",
+      7 : "VII",
+      8 : "VIII",
+      9 : "IX"
+    },
+    {
+      0 : "",
+      1 : "X",
+      2 : "XX",
+      3 : "XXX",
+      4 : "XL",
+      5 : "L",
+      6 : "LX",
+      7 : "LXX",
+      8 : "LXXX",
+      9 : "XC"
+    },
+    {
+      1 : "C",
+      2: "CC",
+      3 : "CCC",
+      4 : "CD",
+      5 : "D",
+      6 : "DC",
+      7 : "DCC",
+      8 : "DCCC",
+      9 : "CM"      
+    },
+    {
+      1 : "M",
+      2 : "MM",
+      3 : "MMM",
+      4 : "MMMM",
+      5 : "MMMMM"
+    }
+  ];
+
+  var digits = [];
+  while (num > 0) {
+      digits[digits.length] = num % 10;
+      num = parseInt(num / 10);
+  }
+ 
+  var roman1,
+    roman10,
+    roman100,
+    roman1000,
+    toDisplay = [];
+
+  if (digits[3] !== undefined) {
+    roman1000 = romanNumbers[3][digits[3]];
+    toDisplay.push(roman1000);
+  } 
+  if (digits[2] !== undefined) {
+    roman100 = romanNumbers[2][digits[2]];
+    toDisplay.push(roman100);
+  }
+  if (digits[1] !== undefined) {
+    roman10 = romanNumbers[1][digits[1]];
+    toDisplay.push(roman10);
+  }
+  if (digits[0] !== undefined) {
+    roman1 = romanNumbers[0][digits[0]];
+    toDisplay.push(roman1);
+  } 
+  return toDisplay.join("");
 }
