df5d0ddb1510a86410a4b95698eba52b0990ca9d
Anett_Soos <soosanett@gmail.com>
2017-06-16 11:17:42 +0100

    save

--- js/main.js
+++ js/main.js
@@ -8,11 +8,37 @@ function whatIsInAName(collection, source) {
   // What's in a name?
   var arr = [];
   // Only change code below this line
-  
+  arr = Object.keys(collection[0]);
   
   // Only change code above this line
   return arr;
+
 }
 
 var result = whatIsInAName([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }], { "a": 1 }) ;
 console.log(result); //should return [{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }]
+
+
+function lookUpProfile(firstName, prop){
+// Only change code below this line
+ var answer;
+
+  for (var i = 0; i < contacts.length; i++ ){
+
+    if (contacts[i].firstName == firstName) {
+        if (contacts[i].hasOwnProperty(prop)){
+           answer = contacts[i][prop];
+           return answer;
+        }else {
+          answer = "No such property";
+          return answer;
+        }
+
+    }else {
+        answer = "No such contact";
+    }    
+  }
+  return answer;
+  
+// Only change code above this line
+}
