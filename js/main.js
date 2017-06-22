83246590775eb26c3ebc0905a58837d617ad8976
Anett_Soos <soosanett@gmail.com>
2017-06-22 15:34:17 +0100

    Revert "interm alg js pairing DNA elements"
    This reverts commit a7568634a0f2afb060aae6daf6834f8ba3ae86b9.

--- js/main.js
+++ js/main.js
@@ -1,54 +1,41 @@
+/*Make a function that looks through an array of objects (first argument)
+and returns an array of all objects that have matching property and value 
+pairs (second argument). Each property and value pair of the source object has to be 
+present in the object from the collection if it is to be included in the returned array.*/
 
-function pairElement(str) {
-  str = str.split("");
-  var result = [];
 
-  for (var i = 0; i < str.length; i++) {
-  	switch (str[i]) {
-  		case "T":
-  			result[i] = ["T","A"];
-  			break;
-  		case "A":
-  			result[i] = ["A", "T"];
-   			break;
-  		case "C":
-  			result[i] = ["C","G"];
-  			break;
-  		case "G":
-  			result[i] = ["G", "C"];
-  			break;
-  	}
-  };
-  
-  return result[2];
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+
+  var sourceSize = Object.keys(source).length;
+  var arrKey = Object.keys(source);
+
+  for (var x = 0; x < collection.length; x++) {
+  	for (var y = 0; y < sourceSize; y++) {
+       var sourceKeys = Object.keys(source);
+       console.log(source[Object.keys(source)]);
+
+	  	// let collectionKeys = Object.keys(collection[i]);
+	  	// if ( collectionKeys[0]  === Object.keys(source)[j] && collection[i][Object.keys(source)] === source[Object.keys(source)] ){
+	  	// 	arr.push(Object.keys(collection[i]));
+	  	// }
+	}
+  }  
+  //console.log(Object.keys(collection[0]));
+  // Only change code above this line
+  return arr;
 }
 
-// function Create2DArray(rows) {
-//   var arr = [];
 
-//   for (var i=0;i<rows;i++) {
-//      arr[i] = ["ted"];
-//   }
-
-//   return arr;
-// }
-// console.log(Create2DArray(3));
-//var test = [["G","C"],["C","G"],["G","C"]];
-
-// [["G", "C"], ["C","G"],["G", "C"]]
-
-var result = pairElement("GCTG");
-console.log("result: " + result);
-
-
-
-
-
-
-
-
-
-
-
-
+//var result = whatIsInAName([{ "a": 1 }, { "a": 1 },{"b": 2}, { "a": 1, "b": 2 }], { "a": 1 }) ;
+//var result = whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 });
+var result = whatIsInAName([
+	{ first: "Romeo", last: "Montague" }, 
+	{ first: "Mercutio", last: null }, 
+	{ first: "Tybalt", last: "Capulet" }
+	],
+	{ last: "Capulet",first: "Tybalt" });
+console.log("result: "+result); //should return [{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }]
 
